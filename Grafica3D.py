a74e3ba0907bde3c4754c3910ca8c2a9fd906539
leov266 <leov266@gmail.com>
2024-06-05 12:40:24 -0400

    Cree la matriz, llame la funcion e hice uso de matplotlib para crear la grafica 3D

--- Grafica3D.py
+++ Grafica3D.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from numpy.linalg import inv
 
 def iteracion_potencia_inversa(matriz_A, num_iteraciones: int):
@@ -11,3 +13,18 @@ def iteracion_potencia_inversa(matriz_A, num_iteraciones: int):
         vector_b_k = vector_b_k1 / norma_vector_b_k1
         lista_vector_b_k.append(vector_b_k)
     return lista_vector_b_k
+
+matriz_A = np.array([[0.5, 0.5], [0.2, 0.8]])
+lista_vector_b_k = iteracion_potencia_inversa(matriz_A, 10)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for i, vector_b_k in enumerate(lista_vector_b_k):
+    ax.scatter(vector_b_k[0], vector_b_k[1], i)
+
+ax.set_xlabel('Componente 1')
+ax.set_ylabel('Componente 2')
+ax.set_zlabel('Iteración')
+plt.title('Evolución del vector b_k en el método de la potencia inversa')
+plt.show()
